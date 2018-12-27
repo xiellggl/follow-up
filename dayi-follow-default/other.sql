c8216adf8eedf32666bb7f35a3278e7f39cc1d5f
xiell <xiell@pvc123.com>
2018-12-27 18:55:26 +0800

    优化首页统计

---
+++ dayi-follow-default/other.sql
@@ -0,0 +1,6 @@
+
+
+#导出线上销售日志为现在的表结构
+SELECT id,flow_id follow_id ,dept_id,open_account_num,in_cash,in_cash_num,out_cash,
+manage_asset_growth manage_growth_fund,manage_asset manage_fund,out_cash_num,
+create_date create_time,modify_date update_time from follow_up_log
