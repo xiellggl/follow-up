c9bb77b6c58f4703b6dec932a53090bfeaf2a787
husm <husm@pvc123.com>
2018-12-29 14:54:28 +0800

    日志管理分页问题

--- dayi-follow-default/src/main/java/com/dayi/follow/controller/OperateLogController.java
+++ dayi-follow-default/src/main/java/com/dayi/follow/controller/OperateLogController.java
@@ -2,6 +2,7 @@ package com.dayi.follow.controller;
 
 import com.dayi.follow.model.follow.OperateLog;
 import com.dayi.follow.service.OperateLogService;
+import com.dayi.follow.util.PageUtil;
 import com.dayi.follow.vo.sys.OperateLogSearchVo;
 import com.dayi.mybatis.support.Page;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author xiell
@@ -25,9 +27,12 @@ public class OperateLogController {
      * 列表查询
      */
     @RequestMapping("/list")
-    public String list(OperateLogSearchVo operateLogSearchVo, Model model) {
+    public String list(HttpServletRequest request, OperateLogSearchVo operateLogSearchVo, Model model) {
         Page<OperateLog> page = operateLogService.searchOperateLog(operateLogSearchVo);
         model.addAttribute("page", page);
+        //分页：当前URL
+        String pageUrl = PageUtil.getPageUrl(request.getRequestURI(), request.getQueryString());  // 构建分页查询请求
+        model.addAttribute("pageUrl", pageUrl);
         return "sys/log/system_log";
     }
 }
