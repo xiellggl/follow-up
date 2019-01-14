2c6d9e2942cdbdf0c866dd153ad1e27ed48c967a
sunny <394001323@qq.com>
2019-01-14 17:33:58 +0800

    修改数据源配置

--- dayi-follow-default/src/main/java/com/dayi/follow/conf/DayiDBConfig.java
+++ dayi-follow-default/src/main/java/com/dayi/follow/conf/DayiDBConfig.java
@@ -2,18 +2,16 @@ package com.dayi.follow.conf;
 
 import com.dayi.mybatis.spring.plus.MybatisSqlSessionFactoryBean;
 import com.dayi.mybatis.spring.plus.plugins.page.PaginationInterceptor;
-import com.dayi.mybatis.spring.spring.SqlSessionFactoryBean;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
@@ -27,21 +25,23 @@ import javax.sql.DataSource;
 @MapperScan(basePackages = {"com.dayi.follow.dao.dayi"},sqlSessionFactoryRef = "dayiSqlSessionFactory")
 public class DayiDBConfig {
 
-    @Autowired
-    @Qualifier("dayiDataSource")
-    DataSource dayiDataSource;
+    @Bean(name = "dayiHikariConfig")
+    @ConfigurationProperties(prefix = "spring.datasource.dayi")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
 
     @Bean(name = "dayiDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.dayi")
-    public DataSource dayiDataSource() {
-        return DataSourceBuilder.create().build();
+    @ConfigurationProperties(prefix="spring.datasource.dayi")
+    public DataSource dayiDataSource(){
+        return new HikariDataSource(hikariConfig());
     }
 
 
     @Bean(name = "dayiSqlSessionFactory")
     public SqlSessionFactory dayiSqlSessionFactory() throws Exception {
         MybatisSqlSessionFactoryBean sqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dayiDataSource);
+        sqlSessionFactoryBean.setDataSource(dayiDataSource());
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         Resource[] resource = resolver.getResources("classpath:com/dayi/follow/dao/dayi/mapper/*.xml");
--- dayi-follow-default/src/main/java/com/dayi/follow/conf/FollowDBConfig.java
+++ dayi-follow-default/src/main/java/com/dayi/follow/conf/FollowDBConfig.java
@@ -2,15 +2,15 @@ package com.dayi.follow.conf;
 
 import com.dayi.mybatis.spring.plus.MybatisSqlSessionFactoryBean;
 import com.dayi.mybatis.spring.plus.plugins.page.PaginationInterceptor;
-import com.dayi.mybatis.spring.spring.SqlSessionFactoryBean;
-import com.dayi.mybatis.support.BaseMapper;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,23 +28,24 @@ import javax.sql.DataSource;
 @MapperScan(basePackages = {"com.dayi.follow.dao.follow"},sqlSessionFactoryRef = "followSqlSessionFactory")
 public class FollowDBConfig {
 
-    @Autowired
-    @Qualifier("followDataSource")
-    DataSource followDataSource;
-
-    @Primary
-    @Bean(name = "followDataSource")
+    @Bean(name = "FollowHikariConfig")
     @ConfigurationProperties(prefix = "spring.datasource.follow")
-    public DataSource followDataSource() {
-        return DataSourceBuilder.create().build();
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
     }
 
+    @Bean(name = "followDataSource")
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource.follow")
+    public DataSource followDataSource(){
+        return new HikariDataSource(hikariConfig());
+    }
 
     @Bean(name = "followSqlSessionFactory")
     @Primary
     public SqlSessionFactory followSqlSessionFactory() throws Exception {
         MybatisSqlSessionFactoryBean sqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(followDataSource);
+        sqlSessionFactoryBean.setDataSource(followDataSource());
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         Resource[] resource = resolver.getResources("classpath:com/dayi/follow/dao/follow/mapper/*.xml");
@@ -55,5 +56,4 @@ public class FollowDBConfig {
 
         return sqlSessionFactoryBean.getObject();
     }
-
 }
