fa2e0d3d89eae0050323045b367d742ed022b39a
xiell <xiell@pvc123.com>
2019-01-18 13:46:36 +0800

    新增修复sql

--- dayi-follow-default/xfix/2019-01-15.sql
+++ dayi-follow-default/xfix/2019-01-15.sql
@@ -1,5 +1,10 @@
 
 
+use dayi_follow;
+
 #修复资产管理中心部门人数
 
-UPDATE department set person_num =52 where id =2
+UPDATE department set person_num =52 where id =2;
+
+#变更首页路径
+UPDATE permission set url ='/index' where id ='23b10044000001a1';
